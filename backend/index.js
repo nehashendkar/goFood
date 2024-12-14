59a0d4bfbbb2a747c0a3b90f95cf6a9381e7037e
nehashendkar <164140050+nehashendkar@users.noreply.github.com>
2024-12-14 12:21:21 +0530

    Update index.js
    dc

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Data Science");
 });
 
 app.use("/api", require("./Routes/CreateUser"));
